2df79ae9207df8ad29b7906b5ca63d1124430f65
Monika <112610179+MonikaRiehn@users.noreply.github.com>
2023-05-30 16:08:19 +0200

    Add files via upload

--- human_resources.sql
+++ human_resources.sql
@@ -110,8 +110,8 @@ SELECT
 	e.id AS emp_id,
 	e.name AS emp_name,
 	e.hire_date,
-	/* The PARTITION BY clause distributes rows into departments (or partitions), specified by e.department_id
-	 * The ORDER BY clause sorts rows in each department by hire_date in ascending order
+	/* The PARTITION BY clause distributes rows into departments (or partitions), specified by e.department_id.
+	 * The ORDER BY clause sorts rows within each department_id by hire_date in ascending order.
 	 * The LEAD() function returns the next_hire_date after the hire_date of the employee before within that department*/
 	LEAD(e.hire_date) OVER (
 		PARTITION BY e.department_id
@@ -119,7 +119,7 @@ SELECT
 			)
 			AS next_hire_date,
 	/* Calculate the duration between the hire date of the current employee and the next employee.
-	 * Note, that next_hire_date is not known as column yet, so you have to repeat the LEAD()-function for the calculation*/
+	 * Note, that next_hire_date is not known as column yet, so you have to repeat the LEAD()-function for the calculation!*/
 	LEAD(e.hire_date) OVER (
     	PARTITION BY e.department_id
 		ORDER BY e.hire_date 
@@ -141,4 +141,31 @@ dept_id|emp_id|emp_name     |hire_date |next_hire_date|duration|
       3|     3|Alice Johnson|2020-01-10|    2023-03-15|    1160|
       3|     6|Dave Davis   |2023-03-15|              |        |
 
+
+----------------------------------------------------------------------
+-- For training purposes:
+-- Find the duration between start of one and the next project
+----------------------------------------------------------------------
+
+SELECT
+  name,
+  start_date,
+  LEAD(start_date) OVER (
+  	ORDER BY start_date
+  	) AS next_start_date,
+  LEAD(start_date) OVER (
+  	ORDER BY start_date
+  	) - start_date AS duration
+FROM
+  projects
+ORDER BY
+  start_date;
+
+ 
+name           |start_date|next_start_date|duration|
+---------------+----------+---------------+--------+
+HR Project 1   |2023-01-01|     2023-02-01|      31|
+IT Project 1   |2023-02-01|     2023-03-01|      28|
+Sales Project 1|2023-03-01|               |        |
+ 
 
